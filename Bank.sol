d7a9318ea2bf6a93f2deff7564e2492dd9532c03
prateek-adhikaree <45109847+prateek-adhikaree@users.noreply.github.com>
2019-06-19 17:45:16 -0400

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -23,43 +23,17 @@ contract Bank {
         
         return true;
     }
-
-    /// @notice Deposit tokens into bank
-    /// @return The balance of the user after the deposit is made
+    
     function deposit() public payable returns (uint) {
         AccountStruct memory acc = balances[msg.sender];
-        acc.currentBalance += msg.value;
-
-        return acc.currentBalance;
-    }
-
-    /// @notice Withdraw token from bank
-    /// @dev This does not return any excess tokens sent to it
-    /// @param withdrawAmount amount you want to withdraw
-    /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
-        AccountStruct memory acc = balances[msg.sender];
-        if(acc.currentBalance >= withdrawAmount) {
-            // Note the way we deduct the balance right away, before sending - due to
-            // the risk of a recursive call that allows the caller to request an amount greater
-            // than their balance
-            acc.currentBalance -= withdrawAmount;
-
-            if (!msg.sender.send(withdrawAmount)) {
-                // increment back only on fail, as may be sending to contract that
-                // has overridden 'send' on the receipt end
-                acc.currentBalance += withdrawAmount;
-            }
+        
+        if (! slabReached(msg.sender)) {
+            acc.currentBalance += msg.value;
         }
-
         return acc.currentBalance;
     }
-
-    /// @notice Get balance
-    /// @return The balance of the user
-    // 'constant' prevents function from editing state variables;
-    function balance() public constant returns (uint) {
-        AccountStruct memory acc = balances[msg.sender];
-        return acc.currentBalance;
+    
+    function slabReached(address accountAddress) internal view returns (bool) {
+        return balances[accountAddress].currentBalance == 100;
     }
 }

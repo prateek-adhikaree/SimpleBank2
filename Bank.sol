3fde187ff5ad8430150270557146e228908b4af5
prateek-adhikaree <45109847+prateek-adhikaree@users.noreply.github.com>
2019-06-19 18:05:15 -0400

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -12,7 +12,7 @@ contract Bank {
     
     mapping (address => AccountStruct) private balances;
     
-    function createAccount(string name) public payable returns (bool) {
+    function createAccount(string name) public returns (bool) {
         AccountStruct acc;
         acc.name = name;
         
